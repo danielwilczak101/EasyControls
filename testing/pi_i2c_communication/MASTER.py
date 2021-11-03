b3eb762badde9001bed1ab01804d9fd31519e514
Jacob Romeo <ravingrossrr@gmail.com>
2021-11-03 09:46:07 -0700

    Uploading the .py file for the master I2C code
    ~ This was successfully tested with the Raspberry Pi 3, acting as MASTER, communicating with the ATmega328P chip, acting as SLAVE via I2C. We turned 2 LEDs on/off communicating via I2C.

---
+++ testing/pi_i2c_communication/MASTER.py
@@ -0,0 +1,31 @@
+#  Raspberry Pi Master for Arduino Slave
+#  i2c_master_pi.py
+#  Connects to Arduino via I2C
+
+#  DroneBot Workshop 2019
+#  https://dronebotworkshop.com
+
+from smbus import SMBus
+
+addr = 0x08 # bus address
+bus = SMBus(1) # indicates /dev/ic2-1
+
+numb = 1
+
+print ("For red, enter 1 for on or 0 for off")
+print ("For green, enter 1 for on or 0 for off")
+print ("Want to exit, enter Return")
+
+while numb == 1:
+	ledstate = input(">>  ") # entering the state of the LED [1, 2, or 0]
+	if ledstate == "1":
+		bus.write_byte_data(addr, 0x01, 1) # turning the red on
+	elif ledstate == "0":
+		bus.write_byte_data(addr, 0x01, 0) # turning the red off
+	elif ledstate == "2":
+		bus.write_byte_data(addr, 0x10, 2) # turning the green on
+	elif ledstate == "0":
+		bus.write_byte_data(addr, 0x10, 0) # turning the green off
+	else:
+		input("Press return to exit") # exiting the program, or stopping the while loop
+		numb = 0
