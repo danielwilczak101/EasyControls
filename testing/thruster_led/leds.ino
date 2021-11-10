ca336c6b56abd4d65d87c42f5918d9b5e3cbd252
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2021-11-10 13:25:26 -0500

    Update leds.ino

--- testing/thruster_led/leds.ino
+++ testing/thruster_led/leds.ino
@@ -1,31 +1,30 @@
 #include <Adafruit_NeoPixel.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1:
-#define LED_PIN    8
+#define LED_PIN    2
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 30
+#define LED_COUNT 8
+
+// Tutorial
+// https://learn.adafruit.com/adafruit-neopixel-uberguide/arduino-library-use
 
 // Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ400);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
 
-void loop() {
+void loop(){
+
+// Turns on one at a time to show a runway effect.
+for (int i = 0; i <= 8; i++) {
+    strip.setPixelColor(i,244, 3, 252);
+    delay(200);
+    strip.show();
+  }
+  
 strip.clear();
-strip.setPixelColor(1, 255, 255, 255);
-strip.setPixelColor(2, 255, 0, 255);
-strip.show();
 }
