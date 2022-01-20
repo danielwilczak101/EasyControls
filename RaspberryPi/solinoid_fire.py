cb81ede29ff9d3935121bd934f4d60e6981ce4fc
AstronautLenny <71563284+AstronautLenny@users.noreply.github.com>
2022-01-20 16:42:52 -0500

    Update solinoid_fire.py

--- RaspberryPi/solinoid_fire.py
+++ RaspberryPi/solinoid_fire.py
@@ -5,7 +5,7 @@ bus = SMBus(1)
 
 class thruster:
 
-    def __init__(self, address, location, orientation, state=0, led_status=0) -> None:
+    def __init__(self, address, location=None, orientation=None, state=0, led_status=0):
         self.address = address
         self.location = location
         self.orientation = orientation
@@ -16,34 +16,40 @@ class thruster:
         """Fires the solinoid as quick as possible
         
         Args: 
-            solinoid - which solinoid you want to fire. Depends on Orientation of Solinoids.
+            solinoid - which solinoid you want to fire. Depends on orientation of solinoids.
             Horizontal (Left, Right) or Vertiacal (Top, Bottom).
                 0 = Left or Bottom
                 1 = Top or Right
         
         """
         bus.write_byte_data(self.address, solinoid, 2)
-        sleep(0.5)
+        sleep(0.05)
     
     def open(self, solinoid):
         """Leaves solinoid open indefinedtly untill otherwise closed"""
         bus.write_byte_data(self.address, solinoid, 1)
-        sleep(0.5)
+        sleep(0.1)
     
     def close(self, solinoid):
         """Leaves solinoid closed indefinedtly untill otherwise opened"""
         bus.write_byte_data(self.address, solinoid, 0)
-        sleep(0.5)
+        sleep(0.1)
 
 
-thruster1 = thruster(0x08, "center", "horizontal")
-thruster2 = thruster(0x09, "bottom left", "vertical")
+thruster = thruster(0x08)
+#thruster1 = thruster(0x08, "center", "horizontal") 
+#thruster2 = thruster(0x09, "bottom left", "vertical")
 
 while True:
 
-    for i in range(50):
-        thruster1.impulse(1)
+    for i in range(15):
+        thruster.impulse(1)
         print("Fire")
-        thruster1.open(1)
-        sleep(3)
-        thruster1.close(1)
+    thruster.open(0)
+    print("Thruster Open")
+    sleep(1)
+    thruster.close(0)
+    print("Thruster Closed")
+        
+        
+
