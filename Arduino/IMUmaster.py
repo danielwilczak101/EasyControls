f1792ef2abf77212379106d4fb5ed97c173e5b20
Jacob Romeo <ravingrossrr@gmail.com>
2022-01-28 20:31:28 -0500

    Added helpful comments

--- Arduino/IMUmaster.py
+++ Arduino/IMUmaster.py
@@ -1,12 +1,14 @@
-import serial
+# Make sure arduino is connected to Pi via USB
+
+import serial # importing the Serial library, install by entering python3 -m pip install pyserial in Pi terminal
 
 print("Waiting on Arduino......")
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-    ser.reset_input_buffer()
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1) # enter ls /dev/tty* into terminal to know what your Serial device name is, baud rate, timeout for read operations
+    ser.reset_input_buffer() # This will flush any byte that could already be in the input buffer at that point
 
-    while True:
+    while True: # infinite loop looking for bytes
         if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
+            line = ser.readline().decode('utf-8').rstrip() # receives and reads all bytes and strings
             print(line)
