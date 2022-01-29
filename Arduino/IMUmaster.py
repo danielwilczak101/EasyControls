1d30c5787f085c30222f27892992f708fd2af14c
Jacob Romeo <ravingrossrr@gmail.com>
2022-01-28 20:18:38 -0500

    Adding code for receiving Serial data for python
    This is a small script for setting up your Pi to basically mirror your Arduino's Serial Monitor.

---
+++ Arduino/IMUmaster.py
@@ -0,0 +1,12 @@
+import serial
+
+print("Waiting on Arduino......")
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
