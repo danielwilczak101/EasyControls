3293417215d7b37d3c42bab5b789ffb87ba8384d
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-09-03 13:53:09 -0400

    Updated async version

--- RaspberryPi/fire_solenoid_jack.py
+++ RaspberryPi/fire_solenoid_jack.py
@@ -2,7 +2,7 @@ from smbus import SMBus
 from time import sleep
 from enum import Enum
 import asyncio
-
+from contextlib import asynccontextmanager
 
 bus = SMBus(1)
 
@@ -14,12 +14,12 @@ class Solinoid(Enum):
 
 class Thruster(Enum):
 
-    ONE = 16
-    TWO = 17
-    THREE = 18
-    FOUR = 19
-    FIVE = 20
-    SIX = 21
+    ONE = 0x10
+    TWO = 0x11
+    THREE = 0x12
+    FOUR = 0x13
+    FIVE = 0x14
+    SIX = 0x15
 
     async def impulse(self, solinoid):
         """Fires the solinoid as quick as possible."""
@@ -36,34 +36,49 @@ class Thruster(Enum):
         bus.write_byte_data(self.value, Solinoid[solinoid.upper()].value, 0)
         await asyncio.sleep(0.01)
 
+    @staticmethod
+    @asynccontextmanager
     async def close_all():
-        await asyncio.gather(*[
-            thruster.close(solinoid.name)
-            for thruster in Thruster
-            for solinoid in Solinoid
-        ])
+        global bus
+        try:
+            yield
+        finally:
+            for _ in range(5):
+                try:
+                    await asyncio.gather(*[
+                        thruster.close(solinoid.name)
+                        for thruster in Thruster
+                        for solinoid in Solinoid
+                    ])
+                except OSError as e:
+                    bus = SMBus(1)
+                    print(e)
+                    await asyncio.sleep(1)
+                else:
+                    break
 
 
 async def main():
     for _ in range(5):
-        await asyncio.gather(
-            Thruster.TWO.open("top"),
-            Thruster.SIX.open("top"),
-            Thruster.FIVE.open("bottom"),
-            Thruster.THREE.open("bottom"),
-        )
 
-        asyncio.sleep(3)
-        Thruster.close_all()
+        async with Thruster.close_all():
+            await asyncio.gather(
+                Thruster.TWO.open("top"),
+                Thruster.SIX.open("top"),
+                Thruster.FIVE.open("bottom"),
+                Thruster.THREE.open("bottom"),
+            )
 
-        await asyncio.gather(
-            Thruster.TWO.open("bottom"),
-            Thruster.SIX.open("bottom"),
-            Thruster.FIVE.open("top"),
-            Thruster.THREE.open("top"),
-        )
+            await asyncio.sleep(3)
 
-        asyncio.sleep(3)
-        Thruster.close_all()
+        async with Thruster.close_all():
+            await asyncio.gather(
+                Thruster.TWO.open("bottom"),
+                Thruster.SIX.open("bottom"),
+                Thruster.FIVE.open("top"),
+                Thruster.THREE.open("top"),
+            )
+
+            await asyncio.sleep(3)
 
 asyncio.run(main())
