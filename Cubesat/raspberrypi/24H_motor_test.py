22644243a30d642a21e5e2163a1a151b63dc4469
Justin Hartland <hartlanj@my.erau.edu>
2022-10-05 20:44:38 -0400

    Coded motor test

---
+++ Cubesat/raspberrypi/24H_motor_test.py
@@ -0,0 +1,24 @@
+from this import d
+import RPi.GPIO as GPIO
+#import time #time.sleep(1) would pause code for 1 second
+
+myPWM = GPIO.PWM(11,20000)
+
+#Define pins
+brakePin = 7
+directionPin = 13
+PWMPin = 33
+
+#Pin Setup
+GPIO.setmode(GPIO.BOARD) #Use board numbering system (1-40)
+myPWM = GPIO.PWM(PWMPin,20000) #Sets PWM frequency to 20 kH
+
+GPIO.setup(brakePin, GPIO.OUT) #Defines this pin as output
+GPIO.setup(directionPin, GPIO.OUT)
+
+#Send high/low through pins
+GPIO.output(brakePin, True) #True means high, False means low
+GPIO.output(directionPin, True)
+
+#Start PWM
+myPWM.start(20) #20% duty cycle
