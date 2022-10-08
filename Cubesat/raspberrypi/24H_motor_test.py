3fed1d3fc67664ffd168062bd9f941df0c2754d8
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-10-08 14:37:11 -0400

    Moved warning removal

--- Cubesat/raspberrypi/24H_motor_test.py
+++ Cubesat/raspberrypi/24H_motor_test.py
@@ -8,12 +8,13 @@ directionPin = 13
 PWMPin = 33
 
 # Pin Setup
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)  # Use board numbering system (1-40)
 
 GPIO.setup(brakePin, GPIO.OUT)  # Defines this pin as output
 GPIO.setup(directionPin, GPIO.OUT)
 GPIO.setup(PWMPin, GPIO.OUT)
-GPIO.setwarnings(False)
+
 
 myPWM = GPIO.PWM(PWMPin, 10000)  # Sets PWM frequency to 20 kH
 
