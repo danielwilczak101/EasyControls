9215904f62bf53102b28ec5596d5ddb8f663ff8c
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-10-08 14:36:12 -0400

    Updated code to handle warning

--- Cubesat/raspberrypi/24H_motor_test.py
+++ Cubesat/raspberrypi/24H_motor_test.py
@@ -1,35 +1,42 @@
 import RPi.GPIO as GPIO
 import time
-#import time #time.sleep(1) would pause code for 1 second
+# import time #time.sleep(1) would pause code for 1 second
 
-#Define pins
+# Define pins
 brakePin = 7
 directionPin = 13
 PWMPin = 33
 
-#Pin Setup
-GPIO.setmode(GPIO.BOARD) #Use board numbering system (1-40)
+# Pin Setup
+GPIO.setmode(GPIO.BOARD)  # Use board numbering system (1-40)
 
-GPIO.setup(brakePin, GPIO.OUT) #Defines this pin as output
+GPIO.setup(brakePin, GPIO.OUT)  # Defines this pin as output
 GPIO.setup(directionPin, GPIO.OUT)
 GPIO.setup(PWMPin, GPIO.OUT)
 GPIO.setwarnings(False)
 
-myPWM = GPIO.PWM(PWMPin,10000) #Sets PWM frequency to 20 kH
+myPWM = GPIO.PWM(PWMPin, 10000)  # Sets PWM frequency to 20 kH
 
-#Send high/low through pins
-GPIO.output(brakePin, True) #True means high, False means low
+# Send high/low through pins
+GPIO.output(brakePin, True)  # True means high, False means low
 GPIO.output(directionPin, False)
 
-#Start PWM
-myPWM.start(100) #0% duty cycle
+# Start PWM
+myPWM.start(100)  # 0% duty cycle
 
-while True:
-    myPWM.ChangeDutyCycle(50)
-    time.sleep(2)
+
+try:
+    for _ in range(5):
+        myPWM.ChangeDutyCycle(50)
+        time.sleep(2)
+        myPWM.ChangeDutyCycle(100)
+        time.sleep(2)
+        myPWM.ChangeDutyCycle(0)
+        time.sleep(2)
+except KeyboardInterrupt:
     myPWM.ChangeDutyCycle(100)
-    time.sleep(2)
-    myPWM.ChangeDutyCycle(0)
-    time.sleep(2)
-    
-        
+except:
+    print("Some error happened")
+finally:
+    myPWM.ChangeDutyCycle(100)
+    GPIO.cleanup()  # cleanup all GPIO
