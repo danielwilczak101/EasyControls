1d47cffe607f768515b3a75cb005e36c4e85803b
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-10-08 15:00:56 -0400

    Updated

--- Cubesat/raspberrypi/24H_motor_test.py
+++ Cubesat/raspberrypi/24H_motor_test.py
@@ -45,9 +45,7 @@ try:
         time.sleep(2)
 except KeyboardInterrupt:
     print("Stop motor and exit.")
-    myPWM.ChangeDutyCycle(0)
-    time.sleep(2)
 except:
     print("Some error happened")
 finally:
-    GPIO.cleanup()  # cleanup all GPIO
+    myPWM.ChangeDutyCycle(0)
--- Cubesat/raspberrypi/install.py
+++ Cubesat/raspberrypi/install.py
@@ -2,7 +2,11 @@
 # when you first power it.
 
 import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
+
+GPIO.cleanup()
+
 gpio_number = 13
 GPIO.setup(gpio_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 print("GPIO no " + str(gpio_number) + ": " + str(GPIO.input(gpio_number)))
