2217a076fa5efaef5cce7ea4550b467f9a092adb
danielwilczak101 <44122838+danielwilczak101@users.noreply.github.com>
2022-10-08 14:45:38 -0400

    Updated

--- Cubesat/raspberrypi/24H_motor_test.py
+++ Cubesat/raspberrypi/24H_motor_test.py
@@ -23,7 +23,7 @@ GPIO.output(brakePin, True)  # True means high, False means low
 GPIO.output(directionPin, False)
 
 # Start PWM
-myPWM.start(100)  # 0% duty cycle
+myPWM.start(0)  # 0% duty cycle
 
 
 try:
