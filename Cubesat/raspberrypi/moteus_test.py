c5f6a3b7d03d85c0890e92f338da4bef9f71673c
Justin Hartland <hartlanj@my.erau.edu>
2022-10-13 15:25:40 -0400

    Added infinity

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -6,10 +6,17 @@ import moteus
 async def main():
     c = moteus.Controller()
 
-    for x in range(2000):
-        print(await c.set_position(position=0.9*math.sin(math.pi*x/500), query=True))
-        await asyncio.sleep(0.001)
+    while True:
+        for x in range(2000):
+            print(await c.set_position(position=0.9*math.sin(math.pi*x/500), query=True))
+            await asyncio.sleep(0.001)
 
-    print(await c.set_stop(query=True))
+        for x in range(2000, 0):
+            print(await c.set_position(position=0.9*math.sin(math.pi*x/500), query=True))
+            await asyncio.sleep(0.001)    
+
+        print(await c.set_stop(query=True))
+
+
 
 asyncio.run(main())        
