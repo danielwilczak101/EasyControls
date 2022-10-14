6b00d3e7c69c1e12949974ce789e478d26325e62
Justin Hartland <hartlanj@my.erau.edu>
2022-10-14 14:46:51 -0400

    chagned

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -9,8 +9,9 @@ async def main():
     await c.set_stop()
 
     while True:
-        for i in range(10):
-            await c.set_position(.5, 1, i/10, query=True)
+        for i in range(100):
+            await c.set_position(.5, 1, i/100, query=True)
+            time.sleep(0.1)
 
         await asyncio.sleep(0.001)
 
