79dd791dcc30d94d03208eb9b2aa606b515958d9
Justin Hartland <hartlanj@my.erau.edu>
2022-10-14 14:39:35 -0400

    updated parameters

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -7,7 +7,7 @@ async def main():
     c = moteus.Controller()
 
     while True:
-        await c.set_position(.5, query=True)
+        await c.set_position(.5, 1, 1, query=True)
 
         await asyncio.sleep(0.001)
 
