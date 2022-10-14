6a0912955b93b1ec745614c51c8484c871f9bce2
Justin Hartland <hartlanj@my.erau.edu>
2022-10-13 23:11:09 -0400

    adsfja

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -3,10 +3,14 @@ import math
 import time
 import moteus
 
-async def main():
+c = moteus.Controller()
+
+c.set_position(.5, query=True)
+""" async def main():
     c = moteus.Controller()
 
     while True:
+
         for x in range(2000):
             print(await c.set_position(position=0.9*math.sin(math.pi*x/500), query=True))
             await asyncio.sleep(0.001)
@@ -19,4 +23,4 @@ async def main():
 
 
 
-asyncio.run(main())        
+asyncio.run(main())         """
