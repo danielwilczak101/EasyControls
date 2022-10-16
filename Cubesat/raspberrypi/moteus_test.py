9a72a45372e0a5bc7a0fffad9dd09d3a4bac78d9
Justin Hartland <hartlanj@my.erau.edu>
2022-10-16 14:54:02 -0400

    dsds

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -10,21 +10,21 @@ async def main():
     c.max_velocity = math.nan
 
     await c.set_stop()
+
     while True:
-        for i in range(2500, 3500):
-            state = await c.set_position(position = math.nan, velocity = 0.01*i + 1, maximum_torque = 1, query=True)
-            print("Velocity:", state.values[moteus.Register.VELOCITY])
+        for i in range(0, 2000):
+            state = await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 1, query=True)
+            print("Actaul Velocity: ", state.values[moteus.Register.VELOCITY])
+            print("Intended Velocity: ", 0.01*i)
             print()
-            print(0.01*i + 1)
             await asyncio.sleep(0.01)
 
-        for i in reversed(range(2500, 3500)):
-            state = await c.set_position(position = math.nan, velocity = 0.01*i + 1, maximum_torque = 1, query=True)
+        for i in reversed(range(0, 2000)):
+            state = await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 1, query=True)
             print("Velocity:", state.values[moteus.Register.VELOCITY])
+            print("Intended Velocity: ", 0.01*i)
             print()
-            print(0.01*i + 1)
             await asyncio.sleep(0.01)     
            
-  
 if __name__ == '__main__':
     asyncio.run(main())
