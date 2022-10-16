b4001731f1adaa3b80e8d9883e90a87ab8b6f225
Justin Hartland <hartlanj@my.erau.edu>
2022-10-16 14:13:08 -0400

    whiled

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -8,16 +8,16 @@ async def main():
     c = moteus.Controller()
 
     await c.set_stop()
-    
-    for i in range(500):
-        await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 1, query=True)
-        print(0.01*i)
-        await asyncio.sleep(0.01)
+    while True:
+        for i in range(500):
+            await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 1, query=True)
+            print(0.01*i)
+            await asyncio.sleep(0.01)
 
-    for i in reversed(range(500)):
-        await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 1, query=True)
-        print(0.01*i)
-        await asyncio.sleep(0.01)     
+        for i in reversed(range(500)):
+            await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 1, query=True)
+            print(0.01*i)
+            await asyncio.sleep(0.01)     
            
   
 
