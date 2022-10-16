09f3e3f13082aa5cebc4bf968027c8fae1738c0d
Justin Hartland <hartlanj@my.erau.edu>
2022-10-16 13:50:48 -0400

    sds

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -7,13 +7,10 @@ async def main():
     c = moteus.Controller()
 
     await c.set_stop()
-
-
-    await c.set_position(position = math.nan, velocity = 10, maximum_torque = 1, query=True)
     
-"""     while True:
+    while True:
         await c.set_position(position = math.nan, velocity = 10, maximum_torque = 1, query=True)
-        await asyncio.sleep(0.01) """
+        await asyncio.sleep(0.01)
 
 """         await c.set_position(position = math.nan, velocity = 5, maximum_torque = 1, query=True)
         await asyncio.sleep(0.1)
