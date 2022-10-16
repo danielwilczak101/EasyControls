bacb6cc29bf263fdf9560b611729482e81e50a26
Justin Hartland <hartlanj@my.erau.edu>
2022-10-16 14:21:58 -0400

    dsd

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -10,12 +10,12 @@ async def main():
     await c.set_stop()
     while True:
         for i in range(1000):
-            await c.set_position(position = math.nan, velocity = 0.01*i + 1, maximum_torque = 1, query=True)
+            state = await c.set_position(position = math.nan, velocity = 0.01*i + 1, maximum_torque = 1, query=True)
             print("Velocity:", state.values[c.Register.VELOCITY])
             await asyncio.sleep(0.01)
 
         for i in reversed(range(1000)):
-            await c.set_position(position = math.nan, velocity = 0.01*i + 1, maximum_torque = 1, query=True)
+            state = await c.set_position(position = math.nan, velocity = 0.01*i + 1, maximum_torque = 1, query=True)
             print("Velocity:", state.values[c.Register.VELOCITY])
             await asyncio.sleep(0.01)     
            
