174c3d73a5650d8b94c7faefc91d632cdee39ae8
Justin Hartland <hartlanj@my.erau.edu>
2022-10-16 13:47:12 -0400

    sdfsd

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -9,15 +9,12 @@ async def main():
     await c.set_stop()
 
     while True:
-        for i in range (3):
-            await c.set_position(position = math.nan, velocity = (i*5), maximum_torque = 1, query=True)
-            await asyncio.sleep(2)
-
-        for i in range (2,0):
-            await c.set_position(position = math.nan, velocity = (i*5), maximum_torque = 1, query=True)
-            await asyncio.sleep(2)    
-
-
-
+        await c.set_position(position = math.nan, velocity = 0, maximum_torque = 1, query=True)
+        await asyncio.sleep(1)
+        await c.set_position(position = math.nan, velocity = 5, maximum_torque = 1, query=True)
+        await asyncio.sleep(1)
+        await c.set_position(position = math.nan, velocity = 10, maximum_torque = 1, query=True)
+        await asyncio.sleep(1)
+  
 
 asyncio.run(main())   
