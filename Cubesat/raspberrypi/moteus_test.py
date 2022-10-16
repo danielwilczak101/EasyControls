001c122afafa35dbf07d650a4b317ce163d828f3
Justin Hartland <hartlanj@my.erau.edu>
2022-10-16 14:10:29 -0400

    dwdw

--- Cubesat/raspberrypi/moteus_test.py
+++ Cubesat/raspberrypi/moteus_test.py
@@ -9,13 +9,13 @@ async def main():
 
     await c.set_stop()
     
-    for i in range(2000):
-        await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 5, query=True)
+    for i in range(500):
+        await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 1, query=True)
         print(0.01*i)
         await asyncio.sleep(0.01)
 
-    for i in reversed(range(2000)):
-        await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 5, query=True)
+    for i in reversed(range(500)):
+        await c.set_position(position = math.nan, velocity = 0.01*i, maximum_torque = 1, query=True)
         print(0.01*i)
         await asyncio.sleep(0.01)     
            
