037688b94b35b4e9d56ea7f4d1c89433f2352551
Justin Hartland <hartlanj@my.erau.edu>
2022-10-17 17:23:25 -0400

    dsd

--- Cubesat/raspberrypi/high_rps_moteus.py
+++ Cubesat/raspberrypi/high_rps_moteus.py
@@ -9,14 +9,16 @@ async def main():
 
     await c.set_stop()
 
+    c.max_velocity = 30
+
     while True:
         for i in range(2500, 4000):
-            state = await c.set_position(position = math.nan, velocity = i*0.01, maximum_torque = 1, maximum_velocity = 30, query=True)
+            state = await c.set_position(position = math.nan, velocity = i*0.01, maximum_torque = 1, query=True)
             print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
             print("Intended Velocity: ", 0.01*i)
             print()
             await asyncio.sleep(0.01)
-            
+
         for i in reversed(range(2500, 4000)):
             state = await c.set_position(position = math.nan, velocity = i*0.01, maximum_torque = 1, maximum_velocity = 30, query=True)
             print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
