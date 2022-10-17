4dc01510183935fdc85ee25b377c0db747f3f5c4
Justin Hartland <hartlanj@my.erau.edu>
2022-10-17 15:52:10 -0400

    max vel

---
+++ Cubesat/raspberrypi/high_rps_moteus.py
@@ -0,0 +1,25 @@
+import asyncio
+import math
+import time
+from xml.etree.ElementTree import TreeBuilder
+import moteus
+
+async def main():
+    c = moteus.Controller()
+
+    c.max_velocity - 40
+
+    await c.set_stop()
+
+    while True:
+        for i in range(0, 4000):
+            state = await c.set_position(position = math.nan, velocity = i/8*0.1, maximum_torque = 1, query=True)
+            print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
+            print("Intended Velocity: ", 0.01*i)
+            print()
+            await asyncio.sleep(0.01)
+
+ 
+           
+if __name__ == '__main__':
+    asyncio.run(main())
