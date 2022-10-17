7703da02967c0c785513ee717609c3989c3d73a6
Justin Hartland <hartlanj@my.erau.edu>
2022-10-17 17:54:10 -0400

    fdfd

--- Cubesat/raspberrypi/Moteus_Test/position_control.py
+++ Cubesat/raspberrypi/Moteus_Test/position_control.py
@@ -10,7 +10,7 @@ async def main():
     await c.set_stop()
 
 
-    for i in range(0, 4, 0.5):
+    for i in reversed(list(range(0, 4))):
         state = await c.set_position(position = i, maximum_torque = 1, query=True)
         print("Position: ", i)
         print()
