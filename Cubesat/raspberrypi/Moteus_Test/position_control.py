0cd3243ca2b718b0fe56abd66c9cd7fd396230b4
Justin Hartland <hartlanj@my.erau.edu>
2022-10-17 17:45:27 -0400

    dsds

--- Cubesat/raspberrypi/Moteus_Test/position_control.py
+++ Cubesat/raspberrypi/Moteus_Test/position_control.py
@@ -9,12 +9,12 @@ async def main():
 
     await c.set_stop()
 
-    while True:
-        for i in range(0, 4, 0.5):
-            state = await c.set_position(position = i, maximum_torque = 1, query=True)
-            print("Position: ", i/8)
-            print()
-            await asyncio.sleep(1)
+
+    for i in range(0, 4, 0.5):
+        state = await c.set_position(position = i, maximum_torque = 1, query=True)
+        print("Position: ", i/8)
+        print()
+        await asyncio.sleep(1)
            
 if __name__ == '__main__':
     asyncio.run(main())
