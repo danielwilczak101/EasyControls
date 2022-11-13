3a079e499cb63dbd2f71f7aef11f50d4620c75bd
Justin Hartland <hartlanj@my.erau.edu>
2022-11-13 13:04:10 -0500

    editted start/stop code

---
+++ Cubesat/raspberrypi/Moteus_Test/Start_Stop.py
@@ -0,0 +1,26 @@
+import asyncio
+import math
+import time
+import moteus
+
+async def main():
+    c = moteus.Controller()
+
+    await c.set_stop()
+
+    while True:
+        for x in range(5000):
+            state = await c.set_position(position = math.nan, velocity = x/10, maximum_torque = 1, query=True)
+            print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
+            print("Intended Velocity: ", x/10)
+            print()
+            await asyncio.sleep(0.01)
+        for x in range(100):
+            state = await c.set_position(position = math.nan, velocity = 0, maximum_torque = 1, query=True)
+            print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
+            print("Intended Velocity: 0")
+            print()
+            await asyncio.sleep(0.01)
+  
+if __name__ == '__main__':
+    asyncio.run(main())
