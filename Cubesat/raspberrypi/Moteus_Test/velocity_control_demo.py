3a918932399c5d21073efadcc16677afaa6688da
Justin Hartland <hartlanj@my.erau.edu>
2022-11-13 12:08:51 -0500

    Made position control sinisoidal

---
+++ Cubesat/raspberrypi/Moteus_Test/velocity_control_demo.py
@@ -0,0 +1,20 @@
+import asyncio
+import math
+import time
+from xml.etree.ElementTree import TreeBuilder
+import moteus
+
+async def main():
+    c = moteus.Controller()
+
+    await c.set_stop()
+
+    while True:
+        state = await c.set_position(position = math.nan, velocity = 10, maximum_torque = 1, query=True)
+        print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
+        print("Intended Velocity: ", 10)
+        print()
+        await asyncio.sleep(0.01)
+  
+if __name__ == '__main__':
+    asyncio.run(main())
