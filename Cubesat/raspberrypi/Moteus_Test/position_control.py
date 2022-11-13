55e1c05c65c732f3450a2408b228c0740d4db62c
Justin Hartland <hartlanj@my.erau.edu>
2022-11-13 12:49:43 -0500

    Confirmed position_control worked

--- Cubesat/raspberrypi/Moteus_Test/position_control.py
+++ Cubesat/raspberrypi/Moteus_Test/position_control.py
@@ -1,14 +1,13 @@
 import asyncio
 import math
 import time
-#from xml.etree.ElementTree import TreeBuilder
 import moteus
 
 async def main():
     c = moteus.Controller()
 
     for x in range(2000):
-        print(await c.seet_position(position=0.9*math.sin(math.pi*x/500), query = True))
+        print(await c.set_position(position=0.9*math.sin(math.pi*x/500), query = True))
         await asyncio.sleep(0.001)
     
     print (await c.set_stop(query = True))
