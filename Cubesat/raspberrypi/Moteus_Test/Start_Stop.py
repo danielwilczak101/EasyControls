fa03fe7c3a8b28b5af549b6aa566d38f4dbda91c
Justin Hartland <hartlanj@my.erau.edu>
2022-11-14 10:24:05 -0500

    edit

--- Cubesat/raspberrypi/Moteus_Test/Start_Stop.py
+++ Cubesat/raspberrypi/Moteus_Test/Start_Stop.py
@@ -22,8 +22,8 @@ async def main():
             print()
             await asyncio.sleep(0.01)
 
-            c.set_brake
-            await asyncio.sleep(0.1)
+        c.set_brake
+        await asyncio.sleep(1)
 
         for x in range(500):
             state = await c.set_position(position = math.nan, velocity = -40, maximum_torque = .5, query=True)
