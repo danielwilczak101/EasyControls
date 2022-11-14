81ae67840421a3523921291f25812c0f23b39dd9
Justin Hartland <hartlanj@my.erau.edu>
2022-11-14 10:17:18 -0500

    max torquee

--- Cubesat/raspberrypi/Moteus_Test/Start_Stop.py
+++ Cubesat/raspberrypi/Moteus_Test/Start_Stop.py
@@ -15,7 +15,7 @@ async def main():
 
     while True:
         for x in range(500):
-            state = await c.set_position(position = math.nan, velocity = 40, maximum_torque = 1, query=True)
+            state = await c.set_position(position = math.nan, velocity = 40, maximum_torque = 5, query=True)
             print("X: ", x)
             print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
             print("Intended Velocity: ", 40)
@@ -23,7 +23,7 @@ async def main():
             await asyncio.sleep(0.01)
 
         for x in range(500):
-            state = await c.set_position(position = math.nan, velocity = -40, maximum_torque = 1, query=True)
+            state = await c.set_position(position = math.nan, velocity = -40, maximum_torque = 5, query=True)
             print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
             print("Intended Velocity: 40")
             print()
