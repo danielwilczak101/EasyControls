8c0a9b31d6fd3e38a1c19a1b49ce63c19977bcff
JustinHartland <justinhartland2@gmail.com>
2022-11-24 15:25:12 -0500

    Updated

--- Cubesat/raspberrypi/Moteus_Test/Start_Stop.py
+++ Cubesat/raspberrypi/Moteus_Test/Start_Stop.py
@@ -1,7 +1,3 @@
-#Goal of this code: To instantly accelerate motor to 75 rps, brake, then instantly accelerate to -75 rps, brake, and repeat
-#Current State: Goal achieved
-#Notes: All though I do not know why, the solution was changing the delay in the brake for loop to 0.02 seconds
- 
 import asyncio
 import math
 import time
@@ -16,13 +12,12 @@ async def main():
 
     for x in range(4):
         for x in range(200):
-            state = await c.set_position(position = math.nan, maximum_torque=60, velocity = speed * direction, accel_limit=300, query=True)
+            state = await c.set_position(position = math.nan, maximum_torque=60, velocity = speed * direction, accel_limit=300, Query=True)
             print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
-            print("Intended Velocity: ", speed*direction) 
+            print("Intended Velocity: ", speed*direction)
             print()
             await asyncio.sleep(0.01)
 
-<<<<<<< HEAD
         for x in range(100):
             c.make_brake()
             await asyncio.sleep(0.02)
@@ -33,14 +28,5 @@ async def main():
 
     await c.set_stop()
 
-=======
-        for x in range(500):
-            state = await c.set_position(position = math.nan, velocity = -40, maximum_torque = .5, query=True)
-            print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
-            print("Intended Velocity: 40")
-            print()
-            await asyncio.sleep(0.01)
-  
->>>>>>> e340de4faf55245504c213ee2e87512e438ff85f
 if __name__ == '__main__':
     asyncio.run(main())
