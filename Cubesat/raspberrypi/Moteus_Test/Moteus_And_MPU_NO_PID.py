ca4321f5bceddee6ea994abcf1c8dff536fb4e86
JustinHartland <justinhartland2@gmail.com>
2022-11-26 15:24:33 -0500

    Cleaned up moteus folder; added motor/mpu script

--- Cubesat/raspberrypi/Moteus_Test/Moteus_And_MPU_NO_PID.py
+++ Cubesat/raspberrypi/Moteus_Test/Moteus_And_MPU_NO_PID.py
@@ -10,15 +10,42 @@ import moteus
 i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = adafruit_mpu6050.MPU6050(i2c)
 
+# Given a point (x, y) return the angle of that point relative to x axis.
+# Returns: angle in degrees
+
+
+def vector_2_degrees(x, y):
+    angle = degrees(atan2(y, x))
+    if angle < 0:
+        angle += 360
+    return angle
+
+
+# Given an accelerometer sensor object return the inclination angles of X/Z and Y/Z
+# Returns: tuple containing the two angles in degrees
+
+
+def get_inclination(_sensor):
+    x, y, z = _sensor.acceleration
+    return vector_2_degrees(x, z), vector_2_degrees(y, z)
+
+
 async def main():
+    direction = 1
     c = moteus.Controller()
 
     await c.set_stop()
 
     while True:
-        state = await c.set_position(position = math.nan, velocity = 0.1, maximum_torque = 1, query=True)
+        angleXZ, angleOfPendulum = get_inclination(sensor)
+        
+        if (angleOfPendulum - 90) <= 0:
+            direction = 1
+        else:
+            direction = -1
+        
+        state = await c.set_position(position = math.nan, velocity = 0.1 * direction, maximum_torque = 1, query=True)
         print("Actual Velocity: ", state.values[moteus.Register.VELOCITY])
-        print("Intended Velocity: ", 1)
         print()
         await asyncio.sleep(0.01)
   
