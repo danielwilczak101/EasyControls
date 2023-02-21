6344293107a8aff1282fcc663c9dcdd10e8372ab
JustinHartland <justinhartland2@gmail.com>
2023-02-21 18:10:50 -0500

    Changed Angle Acquisition Process

--- Cubesat/raspberrypi/Moteus_Test/1DOFControl.py
+++ Cubesat/raspberrypi/Moteus_Test/1DOFControl.py
@@ -8,54 +8,54 @@ import board
 import adafruit_mpu6050
 import moteus
 
-flag = 1
-
 i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = adafruit_mpu6050.MPU6050(i2c)
 
 # Given a point (x, y) return the angle of that point relative to x axis.
 # Returns: angle in degrees
 
-def vector_2_degrees(x, y):
+def vector_2_degrees(flag, x, y):
     angle = degrees(atan2(y, x))
-    if angle < 0:
-        angle += 360
-    return angle
+    if flag == 1:
+        initialAngle = angle
+        flag = 0
+    angle = angle - initialAngle
+    return angle, flag
 
 # Given an accelerometer sensor object return the inclination angles of X/Z and Y/Z
 # Returns: tuple containing the two angles in degrees
 
-def get_inclination(_sensor):
+def get_inclination(flag, _sensor):
     x, y, z = _sensor.acceleration
-    return vector_2_degrees(y, z)
+    return vector_2_degrees(flag, y, z)
 
-def getAngle():
-    while True:
-        angleOfCubeSat = get_inclination(sensor)
-        if flag == 1:
-            initialAngle = angleOfCubeSat
-            flag = 0
-        print("YZ angle = {:6.2f}deg".format(
-            angleOfCubeSat - initialAngle))
-        return angleOfCubeSat
+def getAngle(flag):
+    angleOfCubeSat, flag = get_inclination(flag, sensor)
+    print("YZ angle = {:6.2f}deg".format(
+        angleOfCubeSat))
+    return angleOfCubeSat, flag
         
 
 async def main():
     c = moteus.Controller()
     await c.set_stop()
 
-    Kp = 1
+    flag = 1
+
+    Kp = 0.1
     Ki = 1
     Kd = 1
 
-    desiredAngle = 90
+    desiredAngleRange = 10
+    desiredAngle = 180
 
     while True:
-        error = (desiredAngle - getAngle(sensor))
+        cubeSatAngle, flag = getAngle(flag)
+        error = (desiredAngle - cubeSatAngle)
         motorVelocity = Kp * error
 
-        state = await c.set_position(velocity = motorVelocity, query=True)
-        await asyncio.sleep(0.05)
+        state = await c.set_position(velocity = motorVelocity, accel_limit = 100, query=True)
+        await asyncio.sleep(0.01)
 
 
 
