8dfe872db2fe918926dfb3b5cb256fb44ce6c289
JustinHartland <justinhartland2@gmail.com>
2023-03-24 15:28:00 -0400

    Updated PID code

--- Cubesat/raspberrypi/Moteus_Test/1DOFControl.py
+++ Cubesat/raspberrypi/Moteus_Test/1DOFControl.py
@@ -4,60 +4,31 @@
 import asyncio
 import time
 import math
-from math import atan2, degrees
 import board
 import adafruit_mpu6050
 import moteus
-import matplotlib.pyplot as plt
-
-i2c = board.I2C()  # uses board.SCL and board.SDA
-sensor = adafruit_mpu6050.MPU6050(i2c)
-
-# Given a point (x, y) return the angle of that point relative to x axis.
-# Returns: angle in degrees
-
-def vector_2_degrees(flag, initialAngle, x, y):
-    angle = degrees(atan2(y, x))
-    if angle < 0:
-        angle = angle +360
-    if flag == 1:
-        initialAngle = angle
-        flag = 0
-    if angle <= initialAngle:
-        angle = -1* (angle - initialAngle)
-    else:
-        angle = abs(angle - 360) + initialAngle
-    return angle, flag, initialAngle
-
-# Given an accelerometer sensor object return the inclination angles of X/Z and Y/Z
-# Returns: tuple containing the two angles in degrees
-
-def get_inclination(flag, initialAngle, _sensor):
-    x, y, z = _sensor.acceleration
-    return vector_2_degrees(flag, initialAngle, y, z)
-
-def getAngle(flag, initialAngle):
-    angleOfCubeSat, flag, initialAngle = get_inclination(flag, initialAngle, sensor)
-    return angleOfCubeSat, flag, initialAngle
+import moteus_pi3hat
         
 
 async def main():
     c = moteus.Controller()
+    transport = moteus_pi3hat.Pi3HatRouter()
     await c.set_stop()
-
-    flag = 1
-    initialAngle = 0
-    angleList = []
 
     Kp = -1
     Ki = 1
     Kd = 1
 
     desiredAngleRange = 10
-    desiredAngle = 206
+    desiredAngle = 0
 
     while True:
-        cubeSatAngle, flag, initialAngle = getAngle(flag, initialAngle)
+        result = await transport.cycle([], request_attitude=True)
+        imu_result = [
+            x for x in result
+            if x.id == -1 and isinstance(x, moteus_pi3hat.CanAttitudeWrapper)][0]
+        
+        cubeSatAngle = euler_rad.pitch * (180/math.pi)
 
         error = (desiredAngle - cubeSatAngle)
 
