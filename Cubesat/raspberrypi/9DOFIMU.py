b74e5abcf1be9f6bdb8dc21bb77813c286c8ef9d
Justin Hartland <justinhartland2@gmail.com>
2023-04-03 16:02:24 -0400

    Added code for 9DOF Board

---
+++ Cubesat/raspberrypi/9DOFIMU.py
@@ -0,0 +1,31 @@
+import time
+import board
+import adafruit_lsm9ds1
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+
+sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
+
+while True:
+    # Read acceleration, magnetometer, gyroscope, temperature.
+    accel_x, accel_y, accel_z = sensor.acceleration
+    mag_x, mag_y, mag_z = sensor.magnetic
+    gyro_x, gyro_y, gyro_z = sensor.gyro
+    temp = sensor.temperature
+    # Print values.
+    print(
+        "Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})".format(
+            accel_x, accel_y, accel_z
+        )
+    )
+    print(
+        "Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})".format(mag_x, mag_y, mag_z)
+    )
+    print(
+        "Gyroscope (rad/sec): ({0:0.3f},{1:0.3f},{2:0.3f})".format(
+            gyro_x, gyro_y, gyro_z
+        )
+    )
+    print("Temperature: {0:0.3f}C".format(temp))
+    # Delay for a second.
+    time.sleep(1.0)
